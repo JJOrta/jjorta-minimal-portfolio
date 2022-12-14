c7ad77d0cc66be96437d56e5f7f2059be390c96e
JJOrta <jose.orta3@upr.edu>
2022-12-14 00:06:47 -0400

    Initial Commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: 'Backend System for Email Service',
+    imgUrl: '/assets/PI1.jpg',
+    stack: ['Python', 'Postman', 'SQL', 'Flask', 'React'],
+    link: 'https://github.com/DATABASE-FALL-2022/db-mail2022-los-expertos-de-ciic3011/tree/main',
+  },
+  {
+    title: 'My Portfolio Page',
+    imgUrl: '/assets/PI3.jpg',
+    stack: ['React', 'Tailwind', 'Vite'],
+    link: 'soon',
+  },
+  {
+    title: 'Island Coffee Connoisseur Service',
+    imgUrl: '/assets/PI2.jpg',
+    stack: ['NextJS', 'CSS'],
+    link: 'soon',
+  }
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    year: 'Fall 2022',
+    title: 'NASA Moonbuggy RUM Engineering Team Telemetry Member/Web Developer',
+    duration: 'Currently',
+    details: 'Proving that obtaining important data can help the rest of theteam to improve their faulty concepts, therefore optimizing the design by about 7% every iteration.',
+  },
+  {
+    year: 'Summer 2022',
+    title: 'Project: Training Materials Tracking Tool',
+    duration: '1 Month and 3 weeks',
+    details: 'One of the L&D projects is to implement a process to build Training Materials using ADDIE (analysis, design, development implementation and evaluation) Methodology. With this process we can categorize, prioritize and build training materials, as needed. Key Responsibilities: 1. Build a tracking tool to manage requests for Training Materials 2. Meet and organize meetings with stakeholders 3. Implement ADDIE process using tool 4. Build playbook 5. Pilot tracking tool in selected areas',
+  },
+  {
+    year: 'Fall 2019',
+    title: 'IEEE VTS Project - Programmer',
+    duration: '6 months',
+    details: 'Worked in development of image processing prototype program that detects and graphs deepness of pothole image with LiDAR.',
+  },
+]
